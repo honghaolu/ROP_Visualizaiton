7fbbc42ad165319a13aed41347c730d898c43eaa
honghaolu <160516827+honghaolu@users.noreply.github.com>
2024-10-18 01:23:53 +0800

    Update pred.py

--- pred.py
+++ pred.py
@@ -1,4 +1,4 @@
-#Step 5: Predicting a single photo
+#Step 7: Predicting a single photo
 
 import numpy as np
 import torch
@@ -6,7 +6,7 @@ import cv2
 import os
 from model.LDA_UNet import U_Net_CBAM
 from torchsummary import summary
-
+import matplotlib.pyplot as plt
 def pred(img_path,result_path):
     print("Load model.")
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -22,11 +22,11 @@ def pred(img_path,result_path):
     net.eval()
     print("Load model done.")
     print("Get predict result.")
-    img = cv2.imread(img_path)
-    origin_shape = img.shape
+    origin_img = cv2.imread(img_path)
+    origin_shape = origin_img.shape
     print("Origin image shape:",origin_shape)
     # transform gray image
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img = cv2.cvtColor(origin_img, cv2.COLOR_RGB2GRAY)
     img = cv2.resize(img, (512, 512))
     # Convert to batch 1, channel 1, array size 512 * 512
     img = img.reshape(1, 1, img.shape[0], img.shape[1])
@@ -50,6 +50,19 @@ def pred(img_path,result_path):
     cv2.imwrite(output_file_path, pred)
 
     print(f"Image saved to: {output_file_path}")
+
+    # show result
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(cv2.cvtColor(origin_img, cv2.COLOR_BGR2RGB))
+    plt.title('Fundus image')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(cv2.cvtColor(pred, cv2.COLOR_BGR2RGB))
+    plt.title('Mask image')
+
+    plt.show()
 
 if __name__ == '__main__':
     img_path = 'dataset/predit/48.png'
